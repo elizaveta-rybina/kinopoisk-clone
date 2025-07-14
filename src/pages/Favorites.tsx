f442f8d161b3ab83cadc5b5971048722b98e2d63
elizavetarybina <liza.rybina@inbox.ru>
2025-07-14 17:34:48 +0300

    fixed the layout and added modal

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,19 +1,65 @@
+import type { Movie } from '@/app/api/types'
 import { favoritesStore } from '@/app/store/favorites'
+import { Modal } from '@/shared'
 import { MovieCard } from '@/widgets'
 import { observer } from 'mobx-react-lite'
+import { useState } from 'react'
 
 export const FavoritesPage = observer(() => {
 	const favorites = favoritesStore.favorites
 
-	if (favorites.length === 0) {
-		return <div className='p-6 text-gray-800'>Нет избранных фильмов.</div>
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null)
+	const [isAddingToFavorites, setIsAddingToFavorites] = useState(true)
+
+	const handleFavoriteClick = (movie: Movie) => {
+		setSelectedMovie(movie)
+		setIsAddingToFavorites(!favoritesStore.isFavorite(movie.id))
+		setIsModalOpen(true)
+	}
+
+	const handleConfirmFavorite = () => {
+		if (selectedMovie) {
+			if (isAddingToFavorites) {
+				favoritesStore.addFavorite(selectedMovie)
+			} else {
+				favoritesStore.removeFavorite(selectedMovie.id)
+			}
+		}
+		setIsModalOpen(false)
+		setSelectedMovie(null)
+	}
+
+	const handleCloseModal = () => {
+		setIsModalOpen(false)
+		setSelectedMovie(null)
 	}
 
 	return (
-		<div className='grid pt-20 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6'>
-			{favorites.map(movie => (
-				<MovieCard key={movie.id} movie={movie} />
-			))}
+		<div className='pt-25 px-5 max-w-9/10 min-h-screen text-white mx-auto w-full relative'>
+			<h1 className='text-4xl font-bold text-gray-800 mb-6'>Мое избранное</h1>
+
+			{favorites.length === 0 ? (
+				<p className='text-gray-700 text-2xl'>Нет избранных фильмов</p>
+			) : (
+				<div className='grid grid-cols-[repeat(auto-fill,minmax(220px,1fr))] gap-4'>
+					{favorites.map(movie => (
+						<MovieCard
+							key={movie.id}
+							movie={movie}
+							onFavoriteClick={() => handleFavoriteClick(movie)}
+						/>
+					))}
+				</div>
+			)}
+
+			<Modal
+				isOpen={isModalOpen}
+				onClose={handleCloseModal}
+				onConfirm={handleConfirmFavorite}
+				movieName={selectedMovie?.name || ''}
+				isAdding={isAddingToFavorites}
+			/>
 		</div>
 	)
 })
